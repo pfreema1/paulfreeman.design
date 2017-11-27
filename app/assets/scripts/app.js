7ae85621448bd06c6fe45214fbba7301a416f76b
Paul <pfreema1@gmail.com>
2017-11-27 18:51:58 -0500

    mobile-menu finished

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -2,6 +2,8 @@
 
 
 $(document).ready(function() {
+
+    //contact form
     $("#contact").submit(function(event) {
         console.log("meow");
         event.preventDefault();
@@ -18,4 +20,53 @@ $(document).ready(function() {
 
         });
     });
+
+    //menu animation
+    $(".close-wrapper").on("click", function() {
+        animateMenu();
+    });
+
+    function animateMenu() {
+        $(".close-wrapper").toggleClass("close-wrapper--show");
+        $(".hamburger-container").toggleClass("hamburger-container--move");
+        $(".mobile-logo").toggleClass("mobile-logo--move");
+        $(".logo-inverted-colors").toggleClass("logo-inverted-colors--show");
+        $(".drop-down-menu").toggleClass("drop-down-menu--show");
+    }
+
+    //handle clicking menu buttons - scroll!
+    $("#menu-home-button").on("click", function() {
+        $('html, body').animate({
+            scrollTop: $("#section-home").offset().top
+        }, 2000);
+        animateMenu();
+    });
+
+    $("#menu-portfolio-button").on("click", function() {
+        $('html, body').animate({
+            scrollTop: $("#section-portfolio").offset().top
+        }, 2000);
+        animateMenu();
+    });
+
+    $("#menu-about-button").on("click", function() {
+        $('html, body').animate({
+            scrollTop: $("#section-about").offset().top
+        }, 2000);
+        animateMenu();
+    });
+
+    $("#menu-contact-button").on("click", function() {
+        $('html, body').animate({
+            scrollTop: $("#section-contact").offset().top
+        }, 2000);
+        animateMenu();
+    });
+
+    // function closeMenu() {
+    //     $(".drop-down-menu").removeClass("drop-down-menu--show");
+    //     animateMenu();
+    // }
+
+
 });
