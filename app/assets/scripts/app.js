ef366df755f159b5c7fdda737dc66ea13de5936d
Paul <pfreema1@gmail.com>
2017-12-02 20:44:37 -0500

    hero redone

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -3,6 +3,127 @@
 
 $(document).ready(function() {
 
+    /************** PARTICLES STUFF ************ */
+
+    particlesJS("particles-js", {
+        "particles": {
+          "number": {
+            "value": 100,
+            "density": {
+              "enable": true,
+              "value_area": 800
+            }
+          },
+          "color": {
+            "value": "#ffffff"
+          },
+          "shape": {
+            "type": "circle",
+            "stroke": {
+              "width": 1,
+              "color": "#000000"
+            },
+            "polygon": {
+              "nb_sides": 5
+            },
+            "image": {
+              "src": "",
+              "width": 100,
+              "height": 100
+            }
+          },
+          "opacity": {
+            "value": .5,
+            "random": false,
+            "anim": {
+              "enable": false,
+              "speed": 1,
+              "opacity_min": 0.1,
+              "sync": false
+            }
+          },
+          "size": {
+            "value": 1.5,
+            "random": true,
+            "anim": {
+              "enable": false,
+              "speed": 40,
+              "size_min": 0.1,
+              "sync": false
+            }
+          },
+          "line_linked": {
+            "enable": true,
+            "distance": 150,
+            "color": "#ffffff",
+            "opacity": 0.4,
+            "width": 1
+          },
+          "move": {
+            "enable": true,
+            "speed": 1,
+            "direction": "none",
+            "random": false,
+            "straight": false,
+            "out_mode": "out",
+            "bounce": false,
+            "attract": {
+              "enable": false,
+              "rotateX": 600,
+              "rotateY": 1200
+            }
+          }
+        },
+        "interactivity": {
+          "detect_on": "canvas",
+          "events": {
+            "onhover": {
+              "enable": true,
+              "mode": "repulse"
+            },
+            "onclick": {
+              "enable": true,
+              "mode": "push"
+            },
+            "resize": true
+          },
+          "modes": {
+            "grab": {
+              "distance": 140,
+              "line_linked": {
+                "opacity": 1
+              }
+            },
+            "bubble": {
+              "distance": 400,
+              "size": 40,
+              "duration": 2,
+              "opacity": 8,
+              "speed": 3
+            },
+            "repulse": {
+              "distance": 100,
+              "duration": .4
+            },
+            "push": {
+              "particles_nb": 1
+            },
+            "remove": {
+              "particles_nb": 2
+            }
+          }
+        },
+        "retina_detect": true
+      });
+
+
+
+
+
+
+    /********************** end particles stuff */
+
+
     //contact form
     $("#contact").submit(function(event) {
         console.log("meow");
@@ -101,6 +222,12 @@ $(document).ready(function() {
     //     offset: "80%"
     // });
 
+    var middlePageWaypoint = new Waypoint({
+        element: $("#middlePageTrigger"),
+        handler: function() {
+            $(".overlay-text-wrapper").css('opacity', '.01');
+        }
+    });
 
     setTimeout(function() {
         $(".mobile-menu-container").addClass("mobile-menu-container--show");
